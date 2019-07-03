0961bfac8950d588060a0148a4065d1c9d3def8a
Top Ace <topace336@gmail.com>
2019-07-03 10:47:31 +0800

    Initial Commit

---
+++ LotQual/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LotQual
+//
+//  Created by HeRo Gold on 7/3/19.
+//  Copyright © 2019 HeRo Gold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
